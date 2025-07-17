511986bce8270e750174c78e6b853bdda654a7bb
Arham-Ramay <arhamramay80@gmail.com>
2025-07-17 22:04:14 +0500

    finally

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello I ma Arham updated");
+  res.send("OK FINALLY UPDATED");
 });
 
 app.listen(PORT, () => {
