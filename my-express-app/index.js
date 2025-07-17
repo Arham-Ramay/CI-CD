800d4db8e88136f596c69bfa7f00e8411e364402
Arham-Ramay <arhamramay80@gmail.com>
2025-07-17 21:42:39 +0500

    deployed

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express on AWS EC2! i am arham";
+  res.send("Hello from Express on AWS EC2! i am arham and its deployed");
 });
 
 app.listen(PORT, () => {
