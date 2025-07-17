878028c2ca84f05195d794d226bf84aa9352f704
Arham-Ramay <arhamramay80@gmail.com>
2025-07-17 20:21:16 +0500

    error added

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Second Branch");
+  res.se("Hello from Second Branch";
 });
 
 app.listen(PORT, () => {
